b8627011262c2f3fd7b8881a7a1b3b7d6d18dcb2
lucifer911 <balagmj@gmail.com>
2018-04-26 15:43:22 -0400

    Create swap.sh

---
+++ swap.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+cd /var
+sudo touch swap.img
+sudo chmod 600 swap.img
+sudo dd if=/dev/zero of=/var/swap.img bs=1024k count=2000
+sudo mkswap /var/swap.img
+sudo swapon /var/swap.img
+sudo free
+sudo echo "/var/swap.img none swap sw 0 0" >> /etc/fstab
